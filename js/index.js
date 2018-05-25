27d84a68f7d652c73ef19b55545cbe3b892ceb5a
Bad Man <759051691@qq.com>
2018-05-25 22:04:15 +0800

    增加顶部导航、header、nav、banner图

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
 //侧边栏
 $(function(){
-	//弹出用户框
-	$("#aside .user").mouseenter(function(){
-		$(this).find(".aside_top").fadeIn(10)
-		.find(".span_gone").click(function(){
-			$(this).parent().css({"display":"none"}).parent().unbind("hover");
-		}).end()
-		.find("p").children().eq(0).click(function(){
-			$(".submitBox").css({"display":"block"})
-		})
-	}).mouseleave(function(){
-		$(this).find(".aside_top").fadeOut(10);
-	})
-	//划过改变背景
+	//弹出用户框、客服、回到顶部、划过改变背景
 	$("#aside li").mouseenter(function(){
 		$(this).find(".asid_pic").css({"background-position-x":-22})
+		.end().find(".commonAside").fadeIn(10)
+		.find(".span_gone").click(function(){
+			$(this).parent().css({"display":"none"}).parent().unbind("hover");
+		})
+		.end().find("p").children().eq(0).click(function(){
+			$(".submitBox").css({"display":"block"})
+			.find(".span_gone").click(function(){
+				$(".submitBox").css({"display":"none"});
+			});
+		})	
 	}).mouseleave(function(){
-		$(this).find(".asid_pic").css({"background-position-x":0})
+		$(this).find(".commonAside").fadeOut(10).end()
+		.find(".asid_pic").css({"background-position-x":0})
 	})
-	//划过弹出客服和返回顶部
+	
+	
+	
 	
 })
